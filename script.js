1492f45521fb64c8493d1bb1222ca26579c54322
Sujoy Das <sujoydaspc2023@gmail.com>
2025-04-09 22:59:40 +0600

    add objects & freeze

--- script.js
+++ script.js
@@ -4,34 +4,44 @@
 // async js coding 
 // forEach map filter find indexOf
 
-let arr = [1,2,3,4,5]
+let arr = [1, 2, 3, 4, 5]
 
 let forEachArr = arr.forEach(function (val) {
     console.log(val)
 })
 
-
 let mapArr = arr.map(function (val) {
     return val
 })
 
-
-let filterArr = arr.filter(function(val){
-    if (val > 3) {
-        return true;
-    } else {
-        return false;
-    }
+let filterArr = arr.filter(function (val) {
+    if (val > 3) { return true; } else { return false; }
 })
-
 
 let findArr = arr.find(function (val) {
- if (val === 3) {
-    return val;
- }   
+    if (val === 3) { return val; }
 })
 
-
 arr.indexOf(2)
+arr.length()
+
+
+// objects key value pairs
+const student = {
+    name: "Sujoy Das",
+    roll: 672736,
+    dept: "CST",
+    account: 5000
+}
+
+student.name;
+student['name']
+
+Object.freeze(student) 
+// freeze korle value change hobe na 
+
+
+
+
 
 
