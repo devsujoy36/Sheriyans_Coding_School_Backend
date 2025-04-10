ef28c1e5c643ca3791afbb714abf231ada75932a
Sujoy Das <sujoydaspc2023@gmail.com>
2025-04-10 13:33:51 +0600

    http module

---
+++ httpModule.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer(function (req, res) {
+    res.end("Hello World");
+})
+
+server.listen(port, function () {
+    console.log(`server start in port: ${port}`)
+});
