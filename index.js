46efa7d436a6d3c2d19cd8a6dcb1cc14aba94ada
Sujoy Das <sujoydaspc2023@gmail.com>
2025-04-15 23:31:58 +0600

    alignment

--- index.js
+++ index.js
@@ -13,8 +13,6 @@
  */
 
 
-
-
 const express = require('express');
 const path = require('path');
 const app = express()
@@ -22,9 +20,9 @@ const port = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render("index")
