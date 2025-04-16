b05ab291cf77e63e98f3501945bbabc072e75157
Sujoy Das <sujoydaspc2023@gmail.com>
2025-04-16 23:56:35 +0600

    changes updated "

--- LearnExpress.js
+++ LearnExpress.js
@@ -14,9 +14,10 @@
 const express = require('express');
 const app = express()
 const port = 3000;
-
+const path = require('path');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Middleware.
 app.use((req, res, next) => {
--- fsModule.js
+++ fsModule.js
@@ -5,55 +5,51 @@ const fs = require('fs');
 // ✅ ১. fs.writeFile() – ফাইল তৈরি/লেখা
 // fs.writeFile("Sujoy.txt", "Kuch kuch hota hain", function (err) {
 //     if (err) { console.error(err); }
-//     else { console.log("Done") }
+//     else { console.log("File Create and Write content Successfully") }
 // })
-
 
 // ✅ ২. fs.appendFile() – বিদ্যমান ফাইলে নতুন লেখা যোগ করা
 // fs.appendFile("Sujoy.txt", "\ntum samjho ge nehi", function (err) {
 //     if (err) { console.error(err); }
-//     else { console.log("Done") }
+//     else { console.log("Text Append Successfully") }
 // })
-
 
 // ✅ ৩. fs.readFile() – ফাইল পড়া
 // fs.readFile('Hello.txt', 'utf8', function (err, data) {
 //     if (err) { console.error(err) }
 //     console.log('Data is:', data);
-// });
-
-
-// ✅ ৪. fs.rename() – ফাইলের নাম পরিবর্তন
-// fs.rename("Sujoy.txt", "Hello.txt", function (err) {
-//     if (err) { console.error(err); }
-//     else { console.log("Done") }
 // })
 
-
-// ✅ ৫. fs.unlink() – ফাইল ডিলিট করা
+// ✅ ৪. fs.unlink() – ফাইল ডিলিট করা
 // fs.unlink("fileName", function (err) {
 //     if (err) { console.error(err) }
-//     else { console.log("File Removed") }
+//     else { console.log("File Removed Successfully") }
 // })
 
 
-// ✅ ৬. fs.mkdir() – নতুন ফোল্ডার তৈরি করা
+// ✅ ৫. fs.mkdir() – নতুন ফোল্ডার তৈরি করা
 // fs.mkdir("Sujoy_Das_Official", { recursive: true } , function (err) {
 //     if (err) { console.error(err) }
-//     else { console.log("Folder Create Successfully") }
+//     else { console.log("Folder Created Successfully") }
 // })
 
 
-// ✅ ৭. fs.rmdir() – ফাঁকা ফোল্ডার মুছে ফেলা
+// ✅ ৬. fs.rmdir() – ফাঁকা ফোল্ডার মুছে ফেলা
 // fs.rmdir('folderName', function (err) {
 //     if (err) { console.error(err) }
-//     else { console.log('Folder Removed'); }
+//     else { console.log('Folder Removed Successfully'); }
 // });
+
+// ✅ ৭. fs.rename() – ফাইলের নাম পরিবর্তন
+// fs.rename("Sujoy.txt", "Hello.txt", function (err) {
+//     if (err) { console.error(err); }
+//     else { console.log("File Renamed Successfully") }
+// })
 
 // ✅ fs.copyFile() (Asynchronous version)
 // fs.copyFile('Hello.txt', "Sujoy.txt", function (err) {
 //     if (err) { console.error(err) }
-//     else { console.log("Done") }
+//     else { console.log("Filed Copied Successfully") }
 // })
 
 
